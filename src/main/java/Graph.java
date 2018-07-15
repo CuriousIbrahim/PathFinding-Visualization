a29f09327c3685e6cf15d56b58e9e617fb73823c
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 14:09:22 -0400

    getNode method now checks if parameters (row, column) are out of index

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -2,11 +2,15 @@ import java.util.List;
 
 public class Graph {
 
-
-
     private Node matrix[][];
+    private int rows;
+    private int columns;
 
     public Graph(int row, int column) {
+
+        this.rows = row;
+        this.columns = column;
+
         this.matrix = new Node[row][column];
 
         for (int i = 0; i < row; i++) {
@@ -34,6 +38,9 @@ public class Graph {
             }
         }
 
+        this.rows = highestY;
+        this.columns = highestX;
+
         this.matrix = new Node[highestY][highestX];
 
         for (Node node : nodes) {
@@ -43,17 +50,15 @@ public class Graph {
     }
 
     public Node getNode(int row, int column) {
+        if (row >= this.rows && row < 0 && column >= this.columns && column < 0) {
+            return null;
+        }
         return this.matrix[row][column];
     }
 
     public Node[] getNodesAdjacentTo(int row, int column) {
 
     }
-
-    class
-
-
-
 
 
 }
