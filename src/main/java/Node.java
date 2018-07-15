69c9228c6a3af4d9701d2730318f68b527ecc6cf
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 13:39:54 -0400

    Remove g and h as variables, remove their get and set methods

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -3,14 +3,7 @@ public class Node {
     // x & y - location
     // g - cost from start node
     // h - estimated cost from this to destination
-    private int x, y, g, h;
-
-    public Node(int x, int y, int g, int h) {
-        this.x = x;
-        this.y = y;
-        this.g = g;
-        this.h = h;
-    }
+    private int x, y;
 
     public Node(int x, int y) {
         this.x = x;
@@ -19,20 +12,6 @@ public class Node {
 
     public Node() {
     }
-
-    public void setAll(int x, int y, int g, int h) {
-        this.x = x;
-        this.y = y;
-        this.g = g;
-        this.h = h;
-    }
-
-    public void setAllExceptH(int x, int y, int g) {
-        this.x = x;
-        this.y = y;
-        this.g = g;
-    }
-
 
     public int getX() {
         return x;
@@ -46,22 +25,5 @@ public class Node {
         this.x = x;
         this.y = y;
    }
-
-    public int getG() {
-        return g;
-    }
-
-    public void setG(int g) {
-        this.g = g;
-    }
-
-    public int getH() {
-        return h;
-    }
-
-    public void setH(int h) {
-        this.h = h;
-    }
-
 
 }
