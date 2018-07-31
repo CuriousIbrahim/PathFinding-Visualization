26f210d979db2c4276deeb2875e7bbf443cd4851
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-31 08:17:26 -0400

    Update test and fix visualization in comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,9 +9,11 @@ public class Main {
         List<Node> nodes = new ArrayList<>();
         nodes.add(new Node(0, 0, 1));
         nodes.add(new Node(0, 1, 1));
-        nodes.add(new Node(0, 2, 1)); // End
+        nodes.add(new Node(0, 2, 1));
+        nodes.add(new Node(0, 3, 1));
         nodes.add(new Node(1, 0, 1)); // Start
         nodes.add(new Node(1, 1, 1));
+        nodes.add(new Node(1, 3, 1)); // End
 
 
 
@@ -21,11 +23,11 @@ public class Main {
 
             N = Node; S = Start Node; E = End Node (destination)
 
-            N --- N --- N
-            |     |     |
-            |     |     |
-            |     |     |
-            S     N     E
+            N --- N --- N --- N
+            |     |           |
+            |     |           |
+            |     |           |
+            S --- N           E
 
          */
 
