0f3a4a98c00fda43d801cf1b8c62defd8e96621d
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-08-21 15:24:36 -0400

    Add test to check path, does not work though, I don't know why

--- src/test/java/TestPathFinding.java
+++ src/test/java/TestPathFinding.java
@@ -3,7 +3,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class TestPathFinding {
@@ -18,6 +17,32 @@ public class TestPathFinding {
         assertEquals(aStar.getPathCost(), actual);
 
     }
+    @Test
+    public void checkPath1Path() {
+        AStar aStar = path1();
+
+        List<Node> actual = new ArrayList<>();
+        actual.add(new Node(1, 0, 1));
+        actual.add(new Node(0, 0, 1));
+        actual.add(new Node(0, 1, 1));
+        actual.add(new Node(0, 2, 1));
+        actual.add(new Node(0, 3, 1));
+        actual.add(new Node(1, 3, 1));
+
+        System.out.println();
+
+        // After printing the test and actual, they look the exact same, yet it keeps stating that they are not.
+        // I don't know why!
+        for (int i = 0; i < actual.size(); i++) {
+            System.out.println(aStar.getPath().get(i) + "\t\t\t" + actual.get(i));
+        }
+
+        System.out.println(aStar.getPath().equals(actual));
+
+        assertTrue(aStar.getPath().equals(actual));
+
+    }
+
 
 
     public AStar path1() {
