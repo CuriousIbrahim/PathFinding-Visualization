185b09d29dbe25f9148c5c4e92ee81fb6fa67184
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-08-22 22:12:09 -0400

    Extend Application and override start()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
 
+        launch(args);
 
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage = new Window();
+
+        primaryStage.show();
+
+    }
 }
